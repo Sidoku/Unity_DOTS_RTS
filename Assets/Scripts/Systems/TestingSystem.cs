d2e4df8677834a680c5951f91b82447e21345ce9
Siddharth Singhai <sidsinghai97@gmail.com>
2025-02-08 23:07:00 -0800

    Commit with all files

---
+++ Assets/Scripts/Systems/TestingSystem.cs
@@ -0,0 +1,20 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+
+partial struct TestingSystem : ISystem
+{
+    /*[BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        int unitCount = 0;
+        foreach (RefRW<Zombie> friendly in SystemAPI.Query<RefRW<Zombie>>())
+       {
+           unitCount++;
+       }
+        Debug.Log("unit count: " + unitCount);
+    }*/
+}
